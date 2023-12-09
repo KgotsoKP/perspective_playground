79edee8d9b4858772aa3c894d1fd64c60b81891b
Kgotso Phiri <kgotsokg12@gmail.com>
2023-12-09 15:04:38 +0200

    PP: Implement reset and add trasform computed propety

--- app.js
+++ app.js
@@ -6,5 +6,23 @@ Vue.createApp({
             rotateY: 0,
             rotateZ: 0
         }
+    },
+    methods: {
+        reset() {
+            this.perspective = 100,
+                this.rotateX = 0,
+                this.rotateY = 0,
+                this.rotateZ = 0
+        }
+    },
+    computed: {
+        box() {
+            return {
+                trasform: `perspective(${this.perspective}px)
+                rotateX(${this.rotateX}px)
+                rotateY(${this.rotateX}px)
+                rotateZ(${this.rotateZ}px)`
+            }
+        }
     }
 }).mount('#app')
